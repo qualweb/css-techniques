54474938b0b8208ba84e22fa486db526e80d3d3b
Estriga <fc47839@alunos.fc.ul.pt>
2019-11-20 14:29:19 +0000

    testes t2

---
+++ test/techniques/QW-CSS-T2.spec.js
@@ -0,0 +1,43 @@
+const {
+  configure,
+  executeCSST
+} = require('../../dist/index');
+const {expect} = require('chai');
+const {getDom} = require('@qualweb/get-dom-puppeteer');
+
+describe('Technique QW-CSS-T2', function () {
+  const tests = [
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/css1/em.html',
+      outcome: 'failed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/css1/emImportant.html',
+      outcome: 'passed'
+    }
+  ];
+
+  let i = 0;
+  let lastOutcome = 'inapplicable';
+  for (const test of tests || []) {
+    if (test.outcome !== lastOutcome) {
+      lastOutcome = test.outcome;
+      i = 0;
+    }
+    i++;
+    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
+      it(`should have outcome="${test.outcome}"`, async function () {
+        this.timeout(10 * 1000);
+        const {stylesheets} = await getDom(test.url);
+
+        configure({
+          techniques: ["QW-CSS-T2"]
+        });
+
+        const report = await executeCSST(stylesheets);
+        //console.log("TCL: report.techniques.length", report.techniques['QW-CSS-T1'].metadata);
+        expect(report.techniques['QW-CSS-T2'].metadata.outcome).to.be.equal(test.outcome);
+      });
+    });
+  }
+});
