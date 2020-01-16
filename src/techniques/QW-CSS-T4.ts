b9d69c9ed332d02c801e80bf0c2157fa72ce1b9f
arbsantos <andre13521@gmail.com>
2020-01-16 16:35:33 +0000

    fixed bugs on T4 and T7

--- src/techniques/QW-CSS-T4.ts
+++ src/techniques/QW-CSS-T4.ts
@@ -1,93 +1,103 @@
-'use strict';
+"use strict";
 
-import { CSSTechniqueResult } from '@qualweb/css-techniques';
-import { CSSStylesheet } from '@qualweb/core';
-import css from 'css';
+import { CSSTechniqueResult } from "@qualweb/css-techniques";
+import { CSSStylesheet } from "@qualweb/core";
+import css from "css";
 
-import Technique from './Technique.object';
+import Technique from "./Technique.object";
 
 class QW_CSS_T4 extends Technique {
-
   constructor() {
     super({
-      name: 'Using CSS to control visual presentation of text',
-      code: 'QW-CSS-T4',
-      mapping: 'C22',
-      description: 'The objective of this technique is to demonstrate how CSS can be used to control the visual presentation of text. This will allow users to modify, via the user agent, the visual characteristics of the text to meet their requirement. The text characteristics include aspects such as size, color, font family and relative placement.',
+      name: "Using CSS to control visual presentation of text",
+      code: "QW-CSS-T4",
+      mapping: "C22",
+      description:
+        "The objective of this technique is to demonstrate how CSS can be used to control the visual presentation of text. This will allow users to modify, via the user agent, the visual characteristics of the text to meet their requirement. The text characteristics include aspects such as size, color, font family and relative placement.",
       metadata: {
         target: {
-          element: '*',
-          attributes: 'font-family text-align font-size font-style font-weight color line-height text-transform letter-spacing background-image first-line :first-letter :before :after'
+          element: "*",
+          attributes:
+            "font-family text-align font-size font-style font-weight color line-height text-transform letter-spacing background-image first-line :first-letter :before :after"
         },
-        'success-criteria': [{
-            name: '1.3.1',
-            level: 'A',
-            principle: 'Perceivable',
-            url: 'https://www.w3.org/WAI/WCAG21/Understanding/info-and-relationships'
+        "success-criteria": [
+          {
+            name: "1.3.1",
+            level: "A",
+            principle: "Perceivable",
+            url:
+              "https://www.w3.org/WAI/WCAG21/Understanding/info-and-relationships"
           },
           {
-            name: '1.4.4',
-            level: 'AA',
-            principle: 'Perceivable',
-            url: 'https://www.w3.org/WAI/WCAG21/Understanding/resize-text'
+            name: "1.4.4",
+            level: "AA",
+            principle: "Perceivable",
+            url: "https://www.w3.org/WAI/WCAG21/Understanding/resize-text"
           },
           {
-            name: '1.4.5',
-            level: 'AA',
-            principle: 'Perceivable',
-            url: 'https://www.w3.org/WAI/WCAG21/Understanding/images-of-text'
+            name: "1.4.5",
+            level: "AA",
+            principle: "Perceivable",
+            url: "https://www.w3.org/WAI/WCAG21/Understanding/images-of-text"
           },
           {
-            name: '1.4.9	',
-            level: 'AAA',
-            principle: 'Perceivable',
-            url: 'https://www.w3.org/WAI/WCAG21/Understanding/images-of-text-no-exception'
+            name: "1.4.9	",
+            level: "AAA",
+            principle: "Perceivable",
+            url:
+              "https://www.w3.org/WAI/WCAG21/Understanding/images-of-text-no-exception"
           }
         ],
         related: [],
         url: {
-          'C8' : 'https://www.w3.org/WAI/WCAG21/Techniques/css/C8',
-          'C12': 'https://www.w3.org/WAI/WCAG21/Techniques/css/C12',
-          'C13': 'https://www.w3.org/WAI/WCAG21/Techniques/css/C13',
-          'C14': 'https://www.w3.org/WAI/WCAG21/Techniques/css/C14',
-          'C21': 'https://www.w3.org/WAI/WCAG21/Techniques/css/C21',
-          'SCR34' : 'https://www.w3.org/WAI/WCAG21/Techniques/client-side-script/SCR34'
+          C8: "https://www.w3.org/WAI/WCAG21/Techniques/css/C8",
+          C12: "https://www.w3.org/WAI/WCAG21/Techniques/css/C12",
+          C13: "https://www.w3.org/WAI/WCAG21/Techniques/css/C13",
+          C14: "https://www.w3.org/WAI/WCAG21/Techniques/css/C14",
+          C21: "https://www.w3.org/WAI/WCAG21/Techniques/css/C21",
+          SCR34:
+            "https://www.w3.org/WAI/WCAG21/Techniques/client-side-script/SCR34"
         },
         passed: 0,
         warning: 0,
         failed: 0,
         inapplicable: 0,
-        outcome: '',
-        description: ''
+        outcome: "",
+        description: ""
       },
       results: new Array<CSSTechniqueResult>()
     });
   }
   async execute(styleSheets: CSSStylesheet[]): Promise<void> {
     for (const styleSheet of styleSheets || []) {
-      if(styleSheet.content && styleSheet.content.plain){
-        if (styleSheet.content.plain.includes('font-size')){
-          this.analyseAST(styleSheet.content.parsed, styleSheet.file);
-        }
+      if (styleSheet.content && styleSheet.content.plain) {
+        this.analyseAST(styleSheet.content.parsed, styleSheet.file);
       }
     }
   }
   analyseAST(cssObject: any, fileName: string): void {
-    if (cssObject === undefined ||
-      cssObject['type'] === 'comment' ||
-      cssObject['type'] === 'keyframes' ||
-      cssObject['type'] === 'import'){ // ignore
+    if (
+      cssObject === undefined ||
+      cssObject["type"] === "comment" ||
+      cssObject["type"] === "keyframes" ||
+      cssObject["type"] === "import"
+    ) {
+      // ignore
       return;
     }
-    if (cssObject['type'] === 'rule' || cssObject['type'] === 'font-face' || cssObject['type'] === 'page') {
-      this.loopDeclarations(cssObject, fileName)
+    if (
+      cssObject["type"] === "rule" ||
+      cssObject["type"] === "font-face" ||
+      cssObject["type"] === "page"
+    ) {
+      this.loopDeclarations(cssObject, fileName);
     } else {
-      if (cssObject['type'] === 'stylesheet') {
-        for (const key of cssObject['stylesheet']['rules'] || []) {
+      if (cssObject["type"] === "stylesheet") {
+        for (const key of cssObject["stylesheet"]["rules"] || []) {
           this.analyseAST(key, fileName);
         }
       } else {
-        for (const key of cssObject['rules'] || []) {
+        for (const key of cssObject["rules"] || []) {
           this.analyseAST(key, fileName);
         }
       }
@@ -95,32 +105,56 @@ class QW_CSS_T4 extends Technique {
   }
 
   loopDeclarations(cssObject: any, fileName: string): void {
-    let declarations = cssObject['declarations'];
-    if(declarations){
+    let declarations = cssObject["declarations"];
+    if (declarations) {
       for (const declaration of declarations || []) {
-        if (declaration['property'] && declaration['value'] ) {
-            this.extractInfo(cssObject, declaration, fileName);
+        if (declaration["property"] && declaration["value"]) {
+          this.extractInfo(cssObject, declaration, fileName);
         }
       }
     }
   }
 
   extractInfo(cssObject: any, declaration: any, fileName: string): void {
-    const names = ['font-family', 'text-align', 'font-size',
-    'font-style', 'font-weight', 'color', 'line-height', 'text-transform', 'letter-spacing',
-    'background-image', 'first-line', ':first-letter', ':before', ':after'];
+    const names = [
+      "font-family",
+      "text-align",
+      "font-size",
+      "font-style",
+      "font-weight",
+      "color",
+      "line-height",
+      "text-transform",
+      "letter-spacing",
+      "background-image",
+      "first-line",
+      ":first-letter",
+      ":before",
+      ":after"
+    ];
 
-    if(names.includes(declaration['property'])){
-      super.fillEvaluation('passed', `Element uses CSS properties were used to control the visual presentation of text`,
-        css.stringify({ type: 'stylesheet', stylesheet:{rules: [cssObject]}}),
-        fileName, cssObject['selectors'] ? cssObject['selectors'].toString() : '', cssObject['position'],
-        declaration['property'], declaration['value'], declaration['position']);
+    if (names.includes(declaration["property"])) {
+      super.fillEvaluation(
+        "passed",
+        `Element uses CSS properties were used to control the visual presentation of text`,
+        css.stringify({
+          type: "stylesheet",
+          stylesheet: { rules: [cssObject] }
+        }),
+        fileName,
+        cssObject["selectors"] ? cssObject["selectors"].toString() : "",
+        cssObject["position"],
+        declaration["property"],
+        declaration["value"],
+        declaration["position"]
+      );
     } else {
-      super.fillEvaluation('inapplicable', `Element doesn't uses CSS properties were used to control the visual presentation of text`)
+      super.fillEvaluation(
+        "inapplicable",
+        `Element doesn't uses CSS properties were used to control the visual presentation of text`
+      );
     }
   }
-
 }
-
 
 export = QW_CSS_T4;
--- test/techniques/QW-CSS-T7.spec.js
+++ test/techniques/QW-CSS-T7.spec.js
@@ -1,66 +1,73 @@
-const {
-  CSSTechniques
-} = require('../../dist/index');
-const {expect} = require('chai');
-const puppeteer = require('puppeteer');
-const { getDom } = require('../getDom');
+const { CSSTechniques } = require("../../dist/index");
+const { expect } = require("chai");
+const puppeteer = require("puppeteer");
+const { getDom } = require("../getDom");
 // const {getDom} = require('@qualweb/get-dom-puppeteer');
-describe('Technique QW-CSS-T7', function () {
+describe("Technique QW-CSS-T7", function() {
   const tests = [
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test1.html',
-      outcome: 'failed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test1.html",
+      outcome: "failed"
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test2.html',
-      outcome: 'failed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test2.html",
+      outcome: "failed"
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test3.html',
-      outcome: 'failed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test3.html",
+      outcome: "failed"
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test4.html',
-      outcome: 'failed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test4.html",
+      outcome: "failed"
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test5.html',
-      outcome: 'failed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test5.html",
+      outcome: "failed"
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test6.html',
-      outcome: 'passed'
+      url:
+        "https://accessible-serv.lasige.di.fc.ul.pt/~asantos/CSSTest-F24/test6.html",
+      outcome: "passed"
     }
   ];
   let browser;
-  it("pup open", async function () {
+  it("pup open", async function() {
     browser = await puppeteer.launch();
   });
   let i = 0;
-  let lastOutcome = 'inapplicable';
+  let lastOutcome = "inapplicable";
   for (const test of tests || []) {
     if (test.outcome !== lastOutcome) {
       lastOutcome = test.outcome;
       i = 0;
     }
     i++;
-    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
-      it(`should have outcome="${test.outcome}"`, async function () {
+    describe(`${test.outcome.charAt(0).toUpperCase() +
+      test.outcome.slice(1)} example ${i}`, function() {
+      it(`should have outcome="${test.outcome}"`, async function() {
         this.timeout(10 * 1000);
         // const {stylesheets, mappedDOM} = await getDom(test.url);
-        const {stylesheets, mappedDOM} = await getDom(browser,test.url);
+        const { stylesheets, mappedDOM } = await getDom(browser, test.url);
 
         const cssTechniques = new CSSTechniques({
           techniques: ["QW-CSS-T7"]
         });
 
-        const report = await cssTechniques.execute(stylesheets);
-        expect(report.techniques['QW-CSS-T7'].metadata.outcome).to.be.equal(test.outcome);
+        const report = await cssTechniques.execute(stylesheets, mappedDOM);
+        expect(report.techniques["QW-CSS-T7"].metadata.outcome).to.be.equal(
+          test.outcome
+        );
       });
     });
   }
-  describe(``,  function () {
-    it(`pup shutdown`, async function () {
+  describe(``, function() {
+    it(`pup shutdown`, async function() {
       await browser.close();
     });
   });
